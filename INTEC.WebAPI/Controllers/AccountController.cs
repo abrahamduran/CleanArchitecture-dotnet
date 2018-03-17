1868d4bd033bf3424bd53a3d1a753753e663c66c
Abraham Isaac Durán <aid.mest@gmail.com>
2018-03-17 17:09:35 -0400

    Implement WebAPI

---
+++ INTEC.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using INTEC.Models.ViewModels;
+using INTEC.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace INTEC.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly IUserService userService;
+
+        public AccountController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet("Index")]
+        public JsonResult Index()
+        {
+            var result = userService.GetAll();
+
+            return Json(result.ResultObject);
+        }
+
+        [HttpGet("Detail")]
+        public JsonResult Detail(string id)
+        {
+            var result = userService.Get(id);
+            
+            return Json(result.ResultObject);
+        }
+
+        [HttpPost("Login")]
+        public JsonResult Login(string Username, string Password)
+        {
+            UserViewModel viewModel = new UserViewModel();
+            viewModel.Username = Username;
+            viewModel.Password = Password;
+
+            var result = userService.Login(viewModel);
+
+            return Json(result.ResultObject);
+        }
+
+        [HttpPost("Register")]
+        public JsonResult Register(string Username, string Password, string Email)
+        {
+            UserViewModel viewModel = new UserViewModel();
+            viewModel.Username = Username;
+            viewModel.Password = Password;
+            viewModel.Email = Email;
+            viewModel.Enrollment = "1068864";
+
+            var result = userService.Register(viewModel);
+
+            return Json(result.ResultObject);
+        }
+    }
+}
---
+++ INTEC.WebAPI/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using INTEC.Repository;
+using INTEC.Repository.Framework;
+using INTEC.Services.Implementations;
+using INTEC.Services.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace INTEC.WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IUserService, UserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+        }
+   }
+}
