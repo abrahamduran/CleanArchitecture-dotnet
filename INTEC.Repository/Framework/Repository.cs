e92d9fa37f69a6b04bf9ad0b9339c021435460eb
Abraham Isaac Durán <aid.mest@gmail.com>
2018-03-17 16:39:59 -0400

    Implement repository class with actions to perform basic data operations

--- INTEC.Repository/Framework/Repository.cs
+++ INTEC.Repository/Framework/Repository.cs
@@ -2,44 +2,169 @@
 using System.Linq.Expressions;
 using INTEC.Data;
 using INTEC.Data.Infaestructure;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace INTEC.Repository.Framework
 {
     public class Repository<T> : IRepository<T> where T : BaseEntity
     {
-        public DataResult Delete(T entity)
+        private readonly ApplicationContext context;
+        private DbSet<T> dbSet;
+
+        public Repository(ApplicationContext context)
         {
-            throw new NotImplementedException();
+            if(context == null)
+            {
+                throw new ArgumentNullException("Debes especificar un Contexto a tu Aplicacion");
+            }
+
+            this.context = context;
+            dbSet = context.Set<T>();
         }
 
         public DataResult GetAll()
         {
-            throw new NotImplementedException();
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+                result.Data = dbSet.ToList();
+
+                //SELECT * FROM TABLA
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
         }
 
         public DataResult GetAll(Expression<Func<T, bool>> specification)
         {
-            throw new NotImplementedException();
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+                result.Data = dbSet.Where(specification).ToList();
+
+                //SELECT * FROM TABLA WHERE Nombre = 'Omar Frometa'
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
         }
 
         public DataResult GetById(int id)
         {
-            throw new NotImplementedException();
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+                result.Data = dbSet.Find(id);
+
+                //SELECT * FROM TABLA WHERE Id = @Id
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
         }
 
         public DataResult Insert(T entity)
         {
-            throw new NotImplementedException();
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+
+                dbSet.Add(entity);
+                context.SaveChanges();
+
+                result.Data = entity;
+
+                //INSERT INTO TABLE(Id, UserName, Password) VALUES (1, OFrometa, miClave)
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
         }
 
         public DataResult SaveChanges()
         {
-            throw new NotImplementedException();
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+                result.Data = context.SaveChanges();
+
+               //SAVE ENTITY STATE
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
         }
 
         public DataResult Update(T entity)
         {
-            throw new NotImplementedException();
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+
+                context.SaveChanges();
+
+                result.Data = entity;
+
+                //UDATE TABLE SET Campo1 = OFrometa WHERE Id = @Id
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
+        }
+
+        public DataResult Delete(T entity)
+        {
+            DataResult result = new DataResult();
+
+            try
+            {
+                result.Success = true;
+
+                dbSet.Remove(entity);
+                context.SaveChanges();
+
+                result.Data = entity;
+
+                //DELETE FROM TABLE WHERE Id = @Id
+            }
+            catch (Exception ex)
+            {
+                result.LogError(ex);
+            }
+
+            return result;
         }
     }
 }
