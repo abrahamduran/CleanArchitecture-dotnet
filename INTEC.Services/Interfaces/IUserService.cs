837bc448dc213846bc82e65898c72b52f20e6fec
Abraham Isaac Durán <aid.mest@gmail.com>
2018-03-17 16:53:20 -0400

    Add User service interface

---
+++ INTEC.Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using INTEC.Models.Infraestructure;
+using INTEC.Models.ViewModels;
+using INTEC.Services.Base;
+
+namespace INTEC.Services.Interfaces
+{
+    public interface IUserService : IBaseService<UserViewModel>
+    {
+        ServiceResult Get(string id);
+        ServiceResult Login(UserViewModel viewModel);
+        ServiceResult Register(UserViewModel viewModel);
+    }
+}
